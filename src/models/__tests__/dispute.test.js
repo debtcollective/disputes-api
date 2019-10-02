ebf8b0d97979ed4dfbfa781ca9ff6f7a26bf6ff5
Orlando Del Aguila <orlando@hashlabs.com>
2019-10-02 17:44:13 -0500

    dev: merge dispute tests and fix paths

--- src/models/__tests__/dispute.test.js
+++ src/models/__tests__/dispute.test.js
@@ -1,6 +1,29 @@
 import Dispute from "../dispute";
+import knex from "../../../knex";
 
 describe("Dispute", () => {
+  beforeAll(() => {
+    Dispute.knex(knex);
+  });
+
+  afterAll(() => {
+    knex.destroy();
+  });
+
+  describe("model", () => {
+    it("creates a new record", async () => {
+      const dispute = await Dispute.query().insert({
+        toolId: "1",
+        toolVersion: "v1",
+      });
+
+      expect(dispute.toolId).toEqual("1");
+      expect(dispute.draft).toEqual(true);
+      expect(dispute.createdAt).toBeDefined();
+      expect(dispute.updatedAt).toBeDefined();
+    });
+  });
+
   describe("validations", () => {
     it("validates data when draft is false", async () => {
       const dispute = await Dispute.fromJson({
